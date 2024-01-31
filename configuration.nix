72e2b7ae57d415a0f4182d1795132f93b28fba0f
T0PSH31F <106733950+T0PSH31F@users.noreply.github.com>
2024-01-31 14:34:31 -0800

    Add files via upload
    Nixe, my 1st official upload! hurr-AH!!

---
+++ configuration.nix
@@ -0,0 +1,109 @@
+# bootstrap.nix
+# Designed as first-boot configuration (bootstrap) for impermanence systems
+# Run nixos-generate-config --root=/mnt for hardware scan then overwrite with this as configuration.nix
+# Then you can reboot and get SecureBoot, TPM, /persist, and whatever else set up and use flakes to install
+{ pkgs, lib, ... }: {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.supportedFilesystems = [ "btrfs" "ntfs" ];
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  hardware.enableAllFirmware = true;
+
+  # Enable networking
+  networking.hostName = "LeNiX";
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+  i18n.defaultLocale = "en_US.utf8";
+
+  # Enable the GNOME Desktop Environment with wayland.
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    displayManager.gdm.wayland = true;
+    desktopManager.gnome.enable = true;
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.t0psh31f = {
+    isNormalUser = true;
+    description = "Erik Wright";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      firefox-wayland
+      bind
+      keepassxc
+      vscode
+      git
+      starship
+      pfetch
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    appimage-run
+    neovim
+    kakoune
+    gcc
+    gnupg
+    pinentry-gnome
+    sbctl
+    tpm2-tss
+    git
+    gnome.gnome-tweaks
+    gnome.gpaste
+    gnomeExtensions.night-theme-switcher
+  ];
+
+  fonts.fonts = with pkgs; [
+    ibm-plex
+    merriweather
+    noto-fonts
+    noto-fonts-emoji
+  ];
+
+  # Add env vars
+  environment.sessionVariables = {
+    QT_QPA_PLATFORM = "wayland";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
+
+  services.gnome.gnome-keyring.enable = lib.mkForce false;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  system.stateVersion = "23.11";
+
+  nix.settings.experimental-features = "nix-command flakes";
+
+}
---
+++ flake.nix
@@ -0,0 +1,17 @@
+# /etc/nixos/flake.nix
+{
+  inputs = {
+    # NOTE: Replace "nixos-23.11" with that which is in system.stateVersion of
+    # configuration.nix. You can also use latter versions if you wish to
+    # upgrade.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+  outputs = { self, nixpkgs }: {
+    # NOTE: 'nixos' is the default hostname set by the installer
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      # NOTE: Change this to aarch64-linux if you are on ARM
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
